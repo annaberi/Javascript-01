d2c377abdc259e9c83ae0c8aee8b594122689a28
annaberi <ana.berishvili28b@geolab.edu.ge>
2023-03-20 20:49:38 +0400

    coomit 2

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+// var x = 1;
+// let y = 2;
+// const z = 3;
+// z = 4;
+
+let x=1; //integer
+let y='1'; //string
+let z=true; //boolean
+let a=NaN;//not a number
+let b=null;//null
+let c=undefined; //undefined
+
+console.log(x);
+
+
+console.log(x>xx);
+console.log(x<xx);
+console.log(x==xx);
+console.log(x===xx);
+console.log(x!==);
+
+
+if(z==is true){
+    console.log('z is true');
+} else{
+    console.log('z is false');
+    
+}
